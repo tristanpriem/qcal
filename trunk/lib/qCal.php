aaeb39846ff7b11e8c6073f0722943fda45c1c35
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2007-09-30 23:31:55 +0000

    Made a few interface changes - removed setProperty. I think that addProperty() makes more sense.

--- trunk/lib/qCal.php
+++ trunk/lib/qCal.php
@@ -16,23 +16,29 @@ require_once 'qCal/Component/Abstract.php';
 class qCal extends qCal_Component_Abstract
 {
     /**
-     * Internet standard newline
-     */
-    const LINE_ENDING = "\r\n";
-    /**
      * Version of this library
      *
      * @var string
      */
     const VERSION = '0.01';
     /**
+     * Internet standard newline
+     */
+    const LINE_ENDING = "\r\n";
+    /**
+     * Per RFC
+     */
+    const CONTENT_TYPE = "text/calendar";
+    /**
      * The longest a line can be before it must be folded
      */
     const LINE_FOLD_LENGTH  = 75;
-    const PROPERTY_OPTIONAL = 1; // binary 0001
-    const PROPERTY_ONCE     = 2; // binary 0010
-    const PROPERTY_REQUIRED = 3; // binary 0011
-    
+    /**
+     * Flags for allowed properties / com
+     */
+    const OPTIONAL = 1; // binary 0001
+    const ONCE     = 2; // binary 0010
+    const REQUIRED = 3; // binary 0011
     /**
      * Contains the name of this component
      *
@@ -40,16 +46,8 @@ class qCal extends qCal_Component_Abstract
      */
     protected $_name = 'VCALENDAR';
     // I stole this idea from bennu :(
-    protected $_allowedProperties = array (
-        'CALSCALE' => self::PROPERTY_OPTIONAL | self::PROPERTY_ONCE, // binary 0011
-        'METHOD' => self::PROPERTY_OPTIONAL | self::PROPERTY_ONCE, // binary 0011
-        'PRODID' => self::PROPERTY_REQUIRED | self::PROPERTY_ONCE, // binary 0101
-        'VERSION' => self::PROPERTY_REQUIRED | self::PROPERTY_ONCE // binary 0101
-        // need to allow x-name properties also
-    );
-    protected $_allowedComponents = array (
-    ); 
-    
+    protected $_allowedProperties = array ();
+    protected $_allowedComponents = array (); 
     /**
      * Class constructor - instantiates object
      *
@@ -57,18 +55,14 @@ class qCal extends qCal_Component_Abstract
      */
     public function __construct()
     {
+        $this->_allowedProperties = array(
+            'CALSCALE' => self::OPTIONAL | self::ONCE, // binary 0011
+            'METHOD' => self::OPTIONAL | self::ONCE, // binary 0011
+            'PRODID' => self::REQUIRED | self::ONCE, // binary 0101
+            'VERSION' => self::REQUIRED | self::ONCE // binary 0101
+            // need to allow x-name properties also
+        );
         // can be overwritten
         $this->setProperty('prodid', '-//MC2 Design Group, Inc.//qCal v' . self::VERSION . '//EN');
     }
-    /**
-     * Tells whether a version is valid according to the rfc
-     * @var string $version  The version number we're validating
-     * @return bool
-     * @todo: implement this
-     */
-    public static function isValidVersion($version)
-    {
-        return true;
-    }
-    
 }
