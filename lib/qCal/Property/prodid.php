8326d7857077527f97bb6f74835bc5d38170a82d
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2007-10-02 07:54:18 +0000

    added qCal_Property_Factory class for generating property objects (removal from qCal_Property_Abstract due to irrelevance)

---
+++ lib/qCal/Property/prodid.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * qCal iCalendar library - prodid property
+ * Please read the LICENSE file
+ * @author Luke Visinoni <luke@mc2design.com>
+ * @author Josh Davies <josh@mc2design.com>
+ * @package qCal
+ * @license GNU Lesser General Public License
+ */
+
+require_once 'Abstract.php';
+
+class qCal_Property_prodid extends qCal_Property_Abstract
+{
+    /**
+     * @todo: add body to this method
+     */
+    public function isValid($key)
+    {
+        return true;
+    }
+}
