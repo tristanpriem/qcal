34b566b748547caf69f99f231dd50f67fa496af3
dexdavies <dexdavies@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2007-10-03 18:51:15 +0000

    Mostly comments, nothing changed of real consequence

--- trunk/lib/qCal/Component/Abstract.php
+++ trunk/lib/qCal/Component/Abstract.php
@@ -15,109 +15,109 @@ require_once 'qCal/Property/Factory.php';
 
 abstract class qCal_Component_Abstract
 {
-    const BEGIN = 'BEGIN:';
-    const END = 'END:';
+	const BEGIN = 'BEGIN:';
+	const END = 'END:';
 	protected $_name = null;
 	protected $_properties = array();
 	protected $_components = array();
 	protected $_allowedProperties = array();
 	protected $_allowedComponents = array();
-    /**
-     * Relay initialization to an init() method so that children can do initialization
-     * and not risk forgetting a parent::__construct()
-     * this is just an idea at this point... it may not be necessary - it will only be
-     * necessary if there is something in __construct that needs to be done every time
-     */
-    public function __construct()
-    {
-        $this->init();
-    }
-    /**
-     * Initialize your component. This is where you set the allowable components, 
-     * properties, etc. - see comments in __construct for more info
-     */
-    abstract protected function init();
+	/**
+	 * Relay initialization to an init() method so that children can do initialization
+	 * and not risk forgetting a parent::__construct()
+	 * this is just an idea at this point... it may not be necessary - it will only be
+	 * necessary if there is something in __construct that needs to be done every time
+	 */
+	public function __construct()
+	{
+		$this->init();
+	}
+	/**
+	 * Initialize your component. This is where you set the allowable components, 
+	 * properties, etc. - see comments in __construct for more info
+	 */
+	abstract protected function init();
 	/**
 	 * Add a property for this component. Parameters can only be set if their key
 	 * is in $this->_allowedProperties and if they comply with RFC 2445
 	 * 
 	 * @var name - the property name we are trying to set
-     * @var value - the value of the property
-     */
+	 * @var value - the value of the property
+	 */
 	public function addProperty($name, $value)
 	{
-        if ($this->isValidProperty($name, $value))
-        {
-            $this->_properties[$name] = qCal_Property_Factory::createInstance($name, $value);
-        }
+		if ($this->isValidProperty($name, $value))
+		{
+			$this->_properties[$name] = qCal_Property_Factory::createInstance($name, $value);
+		}
 	}
 	/**
 	 * Retrieve a property from this component
 	 * 
 	 * @var name - the property name we are trying to set
-     */
+	 */
 	public function getProperty($name)
 	{
-        if (array_key_exists($name, $this->_properties))
-        {
-            return $this->_properties[$name];
-        }
-        return null;
+		if (array_key_exists($name, $this->_properties))
+		{
+			return $this->_properties[$name];
+		}
+		return null;
 	}
 	/**
 	 * Verifies a property for this component - first checks that the key is in 
-     * allowedProperties, and then if it is, creates a property object and validates
+	 * allowedProperties, and then if it is, creates a property object and validates
 	 * 
 	 * @var name - the property name we are trying to set
-     * @var value - the value of the property
-     */
-    protected function isValidProperty($name, $value)
-    {
-        // per rfc 2445 - property names are to be capitalized
-        $name = strtoupper($name);
-        // check that property ($name) is allowed to be set on this component
+	 * @var value - the value of the property
+	 */
+	protected function isValidProperty($name, $value)
+	{
+		// per rfc 2445 - property names are to be capitalized
+		$name = strtoupper($name);
+		// check that property ($name) is allowed to be set on this component
 		if (in_array($name, $this->_allowedProperties))
 		{
-            try {
-                $property = qCal_Property_Factory::createInstance($name, $value);
-                return $property->isValid();
-            } catch (qCal_Component_Exception $e) {
-                // @todo: maybe log that this happened so the user can figure it out?
-                // @todo: qCal_Logger - qCal_Logger::setOptions(); qCal_Logger::getLog()
-                return false;
-            }
+			try {
+				$property = qCal_Property_Factory::createInstance($name, $value);
+				return $property->isValid();
+			} catch (qCal_Component_Exception $e) {
+				// @todo: maybe log that this happened so the user can figure it out?
+				// @todo: qCal_Logger - qCal_Logger::setOptions(); qCal_Logger::getLog()
+				return false;
+			}
 		}
-        return false;
-    }
+		return false;
+	}
 	/**
 	 * Add a component for this component. Parameters can only be set if their key
 	 * is in $this->_allowedComponents and if they comply with RFC 2445
 	 * 
 	 * @var name - the component name we are trying to set
-     * @var value - the value of the component
-     */
+	 * @var value - the value of the component
+	 */
 	public function addComponent($name, $value)
 	{
-        $this->_components[$name] = $value;
+		$this->_components[$name] = $value;
 	}
-    public function serialize()
-    {
-        $lines = array();
-        // uppercase the name of this component and add a line ending
-        $lines[] = strtoupper(self::BEGIN . $this->_name);
-        
-        // add this component's
-        foreach ($this->_properties as $property) $lines[] = $property->serialize();
-        foreach ($this->_components as $component) $lines[] = $component->serialize();
-        
-        $lines[] = self::BEGIN . $name;
-        return implode(qCal::LINE_ENDING, $lines);
-    }
-    
-    public function __toString()
-    {
-        header('Content-Type: ' . qCal::CONTENT_TYPE . '; charset=' . qCal::charset());
-        return $this->serialize();
-    }
+	public function serialize()
+	{
+		$lines = array();
+		// uppercase the name of this component and add a line ending
+		$lines[] = strtoupper(self::BEGIN . $this->_name);
+
+		// add this component's (... component's what?-JD)
+		foreach ($this->_properties as $property) $lines[] = $property->serialize();
+		foreach ($this->_components as $component) $lines[] = $component->serialize();
+
+		$lines[] = self::BEGIN . $name;
+		return implode(qCal::LINE_ENDING, $lines);
+	}
+
+	public function __toString()
+	{
+		header('Content-Type: ' . qCal::CONTENT_TYPE . '; charset=' . qCal::charset());
+		return $this->serialize();
+	}
 }	
 
--- trunk/tests/UnitTestCases/TestOfqCalProperties.php
+++ trunk/tests/UnitTestCases/TestOfqCalProperties.php
@@ -7,7 +7,17 @@ class TestOfqCalProperties extends UnitTestCase
     public function testRequiredqCalProperties()
     {
         $cal = new qCal();
-        $cal->addComponent(new qCal());
+	$cal->addComponent(new qCal()); //wouldn't this be adding a 
+					//blank calendar?  i.e., shouldn't
+					//this throw an error of some sort?
+	/**************
+	 * Not sure if these belong here,
+	 * or if they're even remotely the
+	 * right format, hence the mad commenting.
+	 *
+	 * $birthday = new qCalEvent();
+	 * $birthday->setProperty('starttime' $time_event_starts)
+	 * ***********/
         
         echo '<pre>';
         echo $cal;
