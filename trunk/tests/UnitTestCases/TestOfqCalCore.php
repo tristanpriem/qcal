5706b43b11401f64a0235ff848946a821d5a6c28
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2007-10-03 05:39:21 +0000

    Added a few unit tests in the core test case. Experimented with serialization (qCal_Component_Abstract::serialize())

--- trunk/tests/UnitTestCases/TestOfqCalCore.php
+++ trunk/tests/UnitTestCases/TestOfqCalCore.php
@@ -6,4 +6,15 @@ class TestOfqCalCore extends UnitTestCase
         $cal = new qCal();
         $this->assertIsA($cal, 'qCal_Component_Abstract');
     }
+    public function testqCalDefaults()
+    {
+        $cal = new qCal();
+        $this->assertEqual($cal->getProperty('version')->__toString(), '2.0');
+        $this->assertEqual($cal->getProperty('prodid')->__toString(), '-//MC2 Design Group, Inc.//qCal v' . qCal::VERSION . '//EN');
+    }
+    public function testqCalToString()
+    {
+        // test the __toString() method I guess
+        $cal = new qCal();
+    }
 }
