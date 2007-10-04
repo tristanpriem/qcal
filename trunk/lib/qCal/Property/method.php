5728928b7d8e198e0a1ab00ea30aa32b897d2f73
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2007-10-04 07:27:34 +0000

    Added classes representing qCal's calscale and method properties

---
+++ trunk/lib/qCal/Property/method.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * qCal iCalendar library - prodid property
+ * Please read the LICENSE file
+ * @author Luke Visinoni <luke@mc2design.com>
+ * @author Josh Davies <josh@mc2design.com>
+ * @package qCal
+ * @license GNU Lesser General Public License
+ */
+
+require_once 'Abstract.php';
+
+class qCal_Property_method extends qCal_Property_Abstract
+{
+    protected $_name = 'METHOD';
+    /**
+     * Verify that the value in this property conforms with rfc 2445
+     * @returns boolean
+     */
+    protected function evaluateIsValid()
+    {
+        // @todo: find out how to validate this value
+    }
+    protected function format($value)
+    {
+        // @todo: find out how to format this value
+        // formatting comes BEFORE validation fyi - luke
+        return $value;
+    }
+}
