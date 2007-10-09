ff26267e708846181cfbbc13744da27076ba6ac1
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2007-10-09 07:01:51 +0000

    Rearranged a lot of the unit tests - went to full mock for properties.

--- trunk/lib/qCal/Property/MultipleValue.php
+++ trunk/lib/qCal/Property/MultipleValue.php
@@ -1,15 +1,23 @@
 <?php 
-require 'qCal/Property.php';
+require_once 'qCal/Property.php';
 abstract class qCal_Property_MultipleValue extends qCal_Property
 {
     protected $_multiple = true;
     protected $_value = array();
+    public function addValue($value)
+    {
+        $this->_value[] = $value;
+    }
+    public function getValue()
+    {
+        return $this->_value;
+    }
     public function serialize()
     {
         $lines = array();
         foreach ($this->_value as $value)
         {
-            $lines[] = $this->getValue() . ':' . $this->getValue();
+            $lines[] = $this->getType() . ':' . $value;
         }
         return implode(qCal::LINE_ENDING, $lines);
     }
