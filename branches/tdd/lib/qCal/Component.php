ca0e3abeb2e07e5267a274c810720664739668be
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2007-10-27 17:39:27 +0000

    Moved serialize() out of the library. All of its duties are now delegated to renderers. Added qCal_Renderer and you now use render() where you used to use serialize()

--- branches/tdd/lib/qCal/Component.php
+++ branches/tdd/lib/qCal/Component.php
@@ -3,27 +3,11 @@ require_once 'qCal/Attachable.php';
 abstract class qCal_Component extends qCal_Attachable
 {
     /**
-     * All components start with BEGIN:
-     */
-    const BEGIN = 'BEGIN:';
-    /**
-     * All components end with END:
-     */
-    const END = 'END:';
-    /**
      * Return component in serialized form (icalendar format)
      */
-    public function serialize()
+    public function render(qCal_Renderer $renderer = null)
     {
-        // this will contain all the lines in this component's serialization
-        $lines = array();
-        // uppercase the name of this component
-        $lines[] = strtoupper(self::BEGIN . $this->getType());
-        // give me all of my children in serialized form as well
-        foreach ($this->children as $child) $lines[] = $child->serialize();
-        // now wrap it up with an END
-        $lines[] = strtoupper(self::END . $this->getType());
-        // return in right format
-        return implode(qCal::LINE_ENDING, $lines);
+        if (is_null($renderer)) $renderer = new qCal_Renderer_Default;
+        return $renderer->render($this);
     }
 }
--- branches/tdd/lib/qCal/Renderer/Default.php
+++ branches/tdd/lib/qCal/Renderer/Default.php
@@ -1,10 +1,48 @@
 <?php
 class qCal_Renderer_Default extends qCal_Renderer
 {
-    // might change to qCal_Component_vcalendar
-    // @todo: output & test correcte content-type, etc.
-    public function render(qCal_Component $cal)
+    /**
+     * All components start with BEGIN:
+     */
+    const BEGIN = 'BEGIN:';
+    /**
+     * All components end with END:
+     */
+    const END = 'END:';
+    /**
+     * Return component in icalendar format
+     */
+    public function render(qCal_Component $component)
     {
-        return $cal->serialize();
+        // this will contain all the lines in this component's serialization
+        $lines = array();
+        $lines[] = strtoupper(self::BEGIN . $component->getType());
+        
+        foreach ($component->getChildren() as $child)
+        {
+            if ($child instanceof qCal_Component)
+            {
+                $lines[] = $this->render($child);
+            }
+            elseif ($child instanceof qCal_Property)
+            {
+                $lines[] = $this->renderProperty($child);
+            }
+        }
+        
+        $lines[] = strtoupper(self::END . $component->getType());
+        
+        return implode(qCal::LINE_ENDING, $lines);
+    }
+    protected function renderProperty(qCal_Property $property)
+    {
+        return $child->getType() . ':' . $child->getValue();
     }
 }
+
+/**
+strtoupper(self::BEGIN . $child->getType());
+
+
+        
+        **/
