a83ec69f3fee21829621d203aa3b9b681f1d3903
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-11-09 22:15:24 +0000

    added RFC description to base property class

--- trunk/lib/qCal/Property.php
+++ trunk/lib/qCal/Property.php
@@ -1,6 +1,24 @@
 <?php
 /**
- * Base property class
+ * Property
+ * 
+ * A property is the definition of an individual attribute describing a
+ * calendar or a calendar component. A property takes the form defined
+ * by the "contentline" notation defined in section 4.1.1.
+ * 
+ * The following is an example of a property:
+ * 
+ *  DTSTART:19960415T133000Z
+ * 
+ * This memo imposes no ordering of properties within an iCalendar
+ * object.
+ * 
+ * Property names, parameter names and enumerated parameter values are
+ * case insensitive. For example, the property name "DUE" is the same as
+ * "due" and "Due", DTSTART;TZID=US-Eastern:19980714T120000 is the same
+ * as DtStart;TzID=US-Eastern:19980714T120000.
+ * 
+ * qCal_Property
  * There are several properties defined in the icalendar specification.
  * All of those properties should extend this class
  */
