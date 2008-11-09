c6a44821e9e54afe9c27afd5be021b931d6e9e15
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-11-09 09:35:35 +0000

    Added docblock for binary data type

--- lib/qCal/DataType/Binary.php
+++ lib/qCal/DataType/Binary.php
@@ -1,4 +1,41 @@
 <?php
+/**
+ * Value Name: BINARY
+ * 
+ * Purpose: This value type is used to identify properties that contain
+ * a character encoding of inline binary data. For example, an inline
+ * attachment of an object code might be included in an iCalendar
+ * object.
+ * 
+ * Formal Definition: The value type is defined by the following
+ * notation:
+ * 
+ *   binary     = *(4b-char) [b-end]
+ *   ; A "BASE64" encoded character string, as defined by [RFC 2045].
+ * 
+ *   b-end      = (2b-char "==") / (3b-char "=")
+ * 
+ *   b-char = ALPHA / DIGIT / "+" / "/"
+ * 
+ * Description: Property values with this value type MUST also include
+ * the inline encoding parameter sequence of ";ENCODING=BASE64". That
+ * is, all inline binary data MUST first be character encoded using the
+ * "BASE64" encoding method defined in [RFC 2045]. No additional content
+ * value encoding (i.e., BACKSLASH character encoding) is defined for
+ * this value type.
+ * 
+ * Example: The following is an abridged example of a "BASE64" encoded
+ * binary value data.
+ * 
+ *   ATTACH;VALUE=BINARY;ENCODING=BASE64:MIICajCCAdOgAwIBAgICBEUwDQY
+ *    JKoZIhvcNAQEEBQAwdzELMAkGA1UEBhMCVVMxLDAqBgNVBAoTI05ldHNjYXBlI
+ *    ENvbW11bmljYXRpb25zIENvcnBvcmF0aW9uMRwwGgYDVQQLExNJbmZv
+ *      <...remainder of "BASE64" encoded binary data...>
+ * 
+ * qCal_DataType_Binary
+ * This object defines any binary object that may be attached to an
+ * icalendar file.
+ */
 class qCal_DataType_Binary extends qCal_DataType {
 
 	
