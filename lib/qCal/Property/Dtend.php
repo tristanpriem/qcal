6fecef034cd45c41275948ef6eecc7d6ce3bdc06
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-11-10 07:07:33 +0000

    fixed allowedComponents in dtend property

--- lib/qCal/Property/Dtend.php
+++ lib/qCal/Property/Dtend.php
@@ -62,6 +62,6 @@
 class qCal_Property_Dtend extends qCal_Property {
 
 	protected $type = 'DATE-TIME';
-	protected $allowedComponents = array('VTODO');
+	protected $allowedComponents = array('VEVENT','VFREEBUSY');
 
 }
