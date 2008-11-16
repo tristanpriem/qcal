323cadc30d1269e17f4f1865734ccb9d0f72a22d
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-11-16 06:58:46 +0000

    added required properties for timezone

--- trunk/tests/UnitTestCase/Component.php
+++ trunk/tests/UnitTestCase/Component.php
@@ -57,25 +57,34 @@ class UnitTestCase_Component extends UnitTestCase {
 	
 	}
 	/**
+	 * Alarm Component
+	 */
+	/**
 	 * Test that each component gets initialized in accordance with the RFC
 	 * conformance rules
 	 */
-	/**
-	 * Calendar Component
-	 */
 	public function testAlarmInitializeConformance() {
 	
-		// test that prodid is required to initialize an alarm
-		// $this->expectException(new qCal_Exception_Conformance('PRODID property must be specified for component "VCALENDAR"'));
-		// $component = new qCal_Component_Calendar();
+		// test that action is required to initialize an alarm
+		$this->expectException(new qCal_Exception_Conformance('ACTION property must be specified for component "VALARM"'));
+		$component = new qCal_Component_Alarm();
+		// test that trigger is required to initialize an alarm
+		$this->expectException(new qCal_Exception_Conformance('TRIGGER property must be specified for component "VALARM"'));
+		$component = new qCal_Component_Alarm('AUDIO');
 	
 	}
 	/**
-	 * Tests that defaults get set correctly when instantiating	
-	 **/
-	public function testAlarmInitializeDefaults() {
+	 * Timezone Component
+	 */
+	/**
+	 * Test that each component gets initialized in accordance with the RFC
+	 * conformance rules
+	 */
+	public function testTimeZoneInitializeConformance() {
 	
-		//$component = new qCal_Component_Alarm();
+		// test that action is required to initialize an alarm
+		$this->expectException(new qCal_Exception_Conformance('TZID property must be specified for component "VTIMEZONE"'));
+		$component = new qCal_Component_Timezone();
 	
 	}
 	
