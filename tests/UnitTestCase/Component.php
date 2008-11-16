b532608cb21e31d62361f3eab8f20fca054c26ff
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-11-16 06:46:27 +0000

    added factory tests for calendar defaults

--- tests/UnitTestCase/Component.php
+++ tests/UnitTestCase/Component.php
@@ -45,13 +45,37 @@ class UnitTestCase_Component extends UnitTestCase {
 	public function testCalendarInitializeDefaults() {
 	
 		$component = new qCal_Component_Calendar();
-		// pr($component);
 		// test calendar defaults. eventually there will be convenience methods
 		// that will allow you to do $component->prodid() to get and set
 		$this->assertEqual($component->getProperty('prodid')->getValue(), '-//Luke Visinoni//qCal v0.1//EN');
 		$this->assertEqual($component->getProperty('version')->getValue(), '2.0');
-        //$this->expectException(new qCal_Exception_Conformance('PRODID property cannot be specified for component "' . $component->getName() . '"'));
-		//$component->prodid('PRODUCT ID');
+		
+		// do it through factory too
+		$component = qCal_Component::factory('VCALENDAR');
+		$this->assertEqual($component->getProperty('prodid')->getValue(), '-//Luke Visinoni//qCal v0.1//EN');
+		$this->assertEqual($component->getProperty('version')->getValue(), '2.0');
+	
+	}
+	/**
+	 * Test that each component gets initialized in accordance with the RFC
+	 * conformance rules
+	 */
+	/**
+	 * Calendar Component
+	 */
+	public function testAlarmInitializeConformance() {
+	
+		// test that prodid is required to initialize an alarm
+		// $this->expectException(new qCal_Exception_Conformance('PRODID property must be specified for component "VCALENDAR"'));
+		// $component = new qCal_Component_Calendar();
+	
+	}
+	/**
+	 * Tests that defaults get set correctly when instantiating	
+	 **/
+	public function testAlarmInitializeDefaults() {
+	
+		//$component = new qCal_Component_Alarm();
 	
 	}
 	
