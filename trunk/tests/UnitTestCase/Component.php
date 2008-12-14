eb9b4e849dd2e969ba65abe3dc08529bf83e03f7
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-12-14 08:01:23 +0000

    uncommented timezone initialize test

--- trunk/tests/UnitTestCase/Component.php
+++ trunk/tests/UnitTestCase/Component.php
@@ -92,7 +92,7 @@ class UnitTestCase_Component extends UnitTestCase {
 	 * Test that each component gets initialized in accordance with the RFC
 	 * conformance rules
 	 */
-	public function NOSHOWtestTimeZoneInitializeConformance() {
+	public function testTimeZoneInitializeConformance() {
 	
 		// test that action is required to initialize an alarm
 		$this->expectException(new qCal_Exception_MissingProperty('VTIMEZONE component requires TZID property'));
