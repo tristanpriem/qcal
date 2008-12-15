09d6c5da3c0d71aa034b06f2f97f3fe661aa5e81
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-12-15 01:01:56 +0000

    added more binary tests (I've actually never worked with binary data before, so I don't really know how to test it. I did the best I know how)

--- trunk/tests/UnitTestCase/Value.php
+++ trunk/tests/UnitTestCase/Value.php
@@ -7,10 +7,21 @@ class UnitTestCase_Value extends UnitTestCase {
 	/**
 	 * Test that binary data is handled right
 	 */
-	public function testBinary() {
+	public function testBinaryToString() {
 	
 		$value = new qCal_Value_Binary("TEST DATA");
 		$this->assertEqual($value->__toString(), base64_encode("TEST DATA"));
+	
+	}
+	/**
+	 * Test that binary data is handled right
+	 */
+	public function testActualBinary() {
+	
+		$binary = file_get_contents('./files/me.jpg');
+		$value = new qCal_Value_Binary($binary);
+		$this->assertEqual($value->__toString(), base64_encode($binary));
+		$this->assertEqual($value->getValue(), $binary);
 	
 	}
 
