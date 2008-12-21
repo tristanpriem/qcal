42732054f91bc024d2515596e61783e5a8615aee
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-12-21 23:02:42 +0000

    date-time object accepts a date in the constructor

--- tests/UnitTestCase/DateTime.php
+++ tests/UnitTestCase/DateTime.php
@@ -13,5 +13,15 @@ class UnitTestCase_DateTime extends UnitTestCase {
 		$this->assertTrue(($before <= $date->toUnixTimestamp() && $after >= $date->toUnixTimeStamp()));
 	
 	}
+	/**
+	 * The object should allow a unix timestamp in the constructor
+	 */
+	public function testAcceptsUnixTimestamp() {
+	
+		$date = strtotime("04/23/1988 1:00pm");
+		$obj = new qCal_DateTime($date);
+		$this->assertEqual($date, $obj->toUnixTimestamp());
+	
+	}
 
 }
