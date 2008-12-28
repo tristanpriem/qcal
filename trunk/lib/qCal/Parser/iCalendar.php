9aedbd9fc3ae49c489f3b822239fd0b51f794c87
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-12-28 21:08:06 +0000

    removed the old parser, it will not work and it was sloppy, I need to do some kind of recursion

--- trunk/lib/qCal/Parser/iCalendar.php
+++ trunk/lib/qCal/Parser/iCalendar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Default icalendar renderer. Pass a component to the renderer, and it will render it in accordance with rfc 2445
+ * Default icalendar parser (others include xcal parser, etc.)
  * @package qCal
  * @copyright Luke Visinoni (luke.visinoni@gmail.com)
  * @author Luke Visinoni (luke.visinoni@gmail.com)
@@ -8,14 +8,6 @@
  */ 
 class qCal_Parser_iCalendar extends qCal_Parser {
 
-	/**
-	 * This is a queue of component data
-	 */
-	protected $queue = array();
-	/**
-	 * iCal object that gets built during parse
-	protected $iCal;
-	 */
     /**
      * This method will parse raw icalendar data. Eventually I might want to add a parseFromFile() method or something
      */
@@ -28,10 +20,7 @@ class qCal_Parser_iCalendar extends qCal_Parser {
     	    	$this->beginComponent($matches[2]);
     	    } elseif (preg_match("/(END):([a-z]+)/i", $line, $matches)) {
     	    	// match END:COMPONENT
-    	    	if ($cmp = $this->endComponent($matches[2])) {
-    	    		// if endComponent returns a component, then its the last one, so return it
-    	    		return $cmp;
-	    		}
+    	    	$this->endComponent($matches[2]);
 	    	} elseif (preg_match("/([a-z0-9-]+)([;a-z0-9=]+)?:(.+)/i", $line, $matches)) {
 	    		// match PROPERTYNAME;PARAM=paramvalue:PROPERTYVALUE
 	    		$params = array_filter(explode(";", $matches[2]));
@@ -56,14 +45,7 @@ class qCal_Parser_iCalendar extends qCal_Parser {
      */
     protected function beginComponent($name) {
     
-    	$cmpArray = array(
-    		'name' => $name,
-    		'properties' => array(),
-	    	'children' => array(),
-    	);
-    	// echo "BEGINNING $name COMPONENT<BR>";
-    	// add this component to the queue
-    	$this->queue[] = $cmpArray;
+    	
     
     }
     /**
@@ -71,20 +53,7 @@ class qCal_Parser_iCalendar extends qCal_Parser {
      */
     protected function endComponent($name) {
     
-    	// echo "ENDING $name COMPONENT<BR>";
-    	// make most recent component in queue an object (do array_pop) if it's name is $name, otherwise throw an exception
-    	// then add this component in the children of the new most recent in queue
-    	$componentArray = array_pop($this->queue);
-    	$component = qCal_Component::factory($componentArray['name'], $componentArray['properties']);
-    	if (isset($this->queue[count($this->queue)-1])) {
-    		// if there is a parent component, add this to it
-    		$this->queue[count($this->queue)-1]['children'][] = $component;
-		} else {
-			// otherwise, build and return the master component
-			foreach ($componentArray['children'] as $property) $component->attach($property);
-			return $component;
-		}
-    	return false;
+    	
     
     }
     /**
@@ -92,9 +61,7 @@ class qCal_Parser_iCalendar extends qCal_Parser {
      */
     protected function addProperty($name, $value, $params) {
     
-    	// echo strtolower("ADDING $name PROPERTY TO CURRENT COMPONENT WITH " . implode(",", $params) . " as params<BR>");
-    	// add this property to the most recent array in queue
-    	$this->queue[count($this->queue)-1]['properties'][] = qCal_Property::factory($name, $value, $params);
+    	
     
     }
     /**
@@ -102,12 +69,7 @@ class qCal_Parser_iCalendar extends qCal_Parser {
      */
     protected function continueProperty($add) {
     
-    	// echo strtolower("$add<BR>");
-    	// add this value to the end of the most recent item in the queue's most recent property
-    	$propertykey = count($this->queue[count($this->queue)-1]['properties'])-1;
-    	$value = $this->queue[count($this->queue)-1]['properties'][$propertykey]->getValue();
-    	$value .= $add;
-    	$this->queue[count($this->queue)-1]['properties'][$propertykey]->setValue($value);
+    	
     
     }
 
