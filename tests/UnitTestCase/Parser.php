40acb8bbe19f077bd61844c8197af82461ca83c5
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2008-12-29 07:52:00 +0000

    got parser working

--- tests/UnitTestCase/Parser.php
+++ tests/UnitTestCase/Parser.php
@@ -20,6 +20,7 @@ class UnitTestCase_Parser extends UnitTestCase {
     	$data = fread($fh, filesize($fn));
     	$parser = new qCal_Parser_iCalendar();
     	$ical = $parser->parse($data);
+    	pre($ical->render());
     	$this->assertIsA($ical, 'qCal_Component');
     
     }
