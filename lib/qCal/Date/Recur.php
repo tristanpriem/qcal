12aef023730198ee51f21b36c34aef291f273e93
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-01-13 05:25:12 +0000

    added test cases for every note in the RFC. I may need to get some more examples and do tests for them from "RRULE" specification

---
+++ lib/qCal/Date/Recur.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This is a class that is used within qCal_Value_Recur to internally store a recur property
+ */
+class qCal_Date_Recur {
+
+	public function __construct() {
+	
+		
+	
+	}
+
+}
