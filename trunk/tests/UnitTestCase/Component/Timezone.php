264f295488383ea1d6fd8f2cf743ff83ecc6b3f0
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-08-15 12:31:01 +0000

    Added test to make sure addProperty overwrites in timezone component

--- trunk/tests/UnitTestCase/Component/Timezone.php
+++ trunk/tests/UnitTestCase/Component/Timezone.php
@@ -24,6 +24,21 @@ class UnitTestCase_Component_Timezone extends UnitTestCase {
 		$tz->addProperty(new qCal_Property_Tzid('New_York-New_York'));
 		$tzid = $tz->getProperty('tzid');
 		$this->assertEqual(count($tzid), 1);
+		$this->assertEqual($tzid[0]->getValue(), 'New_York-New_York');
+	
+	}
+	/**
+	 *                ; 'last-mod' and 'tzurl' are optional,
+	 *              but MUST NOT occur more than once
+	 */
+	public function testLastModAndTzurlMustNotOccurMoreThanOnce() {
+	
+		$tz = new qCal_Component_Vtimezone(array(
+			'tzid' => 'California-Los_Angeles',
+			'last-modified' => new qCal_Date(time()),
+			'tzurl' => 'http://www.example.com',
+		));
+		//$tz->addProperty();
 	
 	}
 	
