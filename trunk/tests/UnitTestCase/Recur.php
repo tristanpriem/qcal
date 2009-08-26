e7407240fde6a0f931c9949fd00599e0ff15799c
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-08-26 04:33:51 +0000

    qCal_Date_Recur:: and qCal_Date_Recur:: can not both be specified in a recurrence rule object

---
+++ trunk/tests/UnitTestCase/Recur.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This tests the qCal_Date_Recur class thoroughly...
+ */
+class UnitTestCase_Recur extends UnitTestCase {
+
+	public function setUp() {
+	
+		// set up and stuff
+	
+	}
+	
+	public function tearDown() {
+	
+		// mr. gorbachev, tear down this wall!!
+	
+	}
+	
+	public function testGetInstancesStartMustComeBeforeEnd() {
+	
+		$recur = new qCal_Date_Recur('yearly');
+		$recur->interval(1);
+		$this->expectException(new qCal_Date_Exception_InvalidRecur('Start date must come before end date'));
+		$dates = $recur->getInstances('08/01/2009', '07/01/2009');
+	
+	}
+	
+	public function testGetInstancesRequiresInterval() {
+	
+		$recur = new qCal_Date_Recur('yearly');
+		$this->expectException(new qCal_Date_Exception_InvalidRecur('You must specify an interval'));
+		$dates = $recur->getInstances('08/01/2009', '09/01/2009');
+	
+	}
+	
+	public function testInvalidFrequency() {
+	
+		$this->expectException(new qCal_Date_Exception_InvalidRecur('"FOO" is not a valid frequency, must be one of the following: SECONDLY, MINUTELY, HOURLY, DAILY, WEEKLY, MONTHLY, YEARLY'));
+		$recur = new qCal_Date_Recur('FOO');
+	
+	}
+	
+	public function testGetters() {
+	
+		$recur = new qCal_Date_Recur('yearly');
+		$recur->count(10)
+			->byMonth(2)
+			->byDay('TU');
+		$this->assertEqual($recur->count(), 10);
+		$this->assertEqual($recur->byMonth(), array(2));
+		$this->assertEqual($recur->byDay(), array('TU'));
+		$this->assertEqual($recur->byMonthDay(), null);
+	
+	}
+	
+	public function testCanHaveCountOrUntilButNotBoth() {
+	
+		$rule = new qCal_Date_Recur('hourly');
+		$this->expectException(new qCal_Date_Exception_InvalidRecur('A recurrence count and an until date cannot both be specified'));
+		$rule->count(10)
+			->until('02/12/2009');
+	
+	}
+	
+	public function testBuildRule() {
+	
+		$recur = new qCal_Date_Recur('yearly');
+		$recur->interval(2) // every other year
+			->byMonth(1) // every other year in january
+			->byDay('SU') // every sunday in january of every other year
+			->byHour(array(8,9)) // every sunday in january of every other year at 8am and 9am
+			->byMinute(30); // every sunday in january of every other year at 8:30am and 9:30am
+		$start = '08/24/1995';
+		$end = '08/24/2009';
+		$dates = $recur->getInstances($start, $end);
+		// pr($dates); // should return an array of qCal_Dates that represent every instance in the timespan
+		
+	}
+
+}
