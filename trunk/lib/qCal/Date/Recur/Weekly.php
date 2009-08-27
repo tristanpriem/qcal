252d358ea77b3ac1a57ed15dad440764f083d587
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-08-27 06:30:04 +0000

    Added weekly recur loop helper

--- trunk/lib/qCal/Date/Recur/Weekly.php
+++ trunk/lib/qCal/Date/Recur/Weekly.php
@@ -1,5 +1,5 @@
 <?php
-class qCal_Date_Recur_Daily extends qCal_Date_Recur_Helper {
+class qCal_Date_Recur_Weekly extends qCal_Date_Recur_Helper {
 
 	/**
 	 * 
@@ -7,18 +7,18 @@ class qCal_Date_Recur_Daily extends qCal_Date_Recur_Helper {
 	public function increment($increment) {
 	
 		$increment = (integer) $increment;
-		$this->datetime->modify("+$increment days");
+		$this->datetime->modify("+$increment weeks");
 	
 	}
 	/**
-	 * 
+	 * @todo This probably isn't right every time since I used the day here... I dunno. I'm tired.
 	 */
 	public function onOrBefore($date) {
 	
 		$date = new qCal_Date($date);
-		// we can return true right off the bat if BEFORE, but same day takes a bit of work
+		// we can return true right off the bat if BEFORE, but same week takes a bit of work
 		if ($this->datetime->time() < $date->time()) return true;
-		// if the date falls within the day or before, return true
+		// if the date falls within the week or before, return true
 		$day = $date->format('m/d/Y');
 		if ($this->datetime->format('m/d/Y') == $day) {
 			return true;
