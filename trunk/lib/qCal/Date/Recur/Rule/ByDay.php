7a7b73c1ceb9d7c109a387052405998ba1d58d96
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-09-02 06:38:20 +0000

    Just added some notes on how I plan to loop over dates to find recurrences

---
+++ trunk/lib/qCal/Date/Recur/Rule/ByDay.php
@@ -0,0 +1,10 @@
+<?php
+class qCal_Date_Recur_Rule_ByDay extends qCal_Date_Recur_Rule {
+
+	public function getRecurrences() {
+	
+		// do the magic!
+	
+	}
+
+}
---
+++ trunk/lib/qCal/Date/Recur/Rule/ByYearDay.php
@@ -0,0 +1,10 @@
+<?php
+class qCal_Date_Recur_Rule_ByYearDay extends qCal_Date_Recur_Rule {
+
+	public function getRecurrences() {
+	
+		// do the magic!
+	
+	}
+
+}
