955b066b90bf5c64db0255d2d06ae12a0bb496af
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-12-19 07:49:23 +0000

    Added timezone getters

--- tests/UnitTestCase/Timezone.php
+++ tests/UnitTestCase/Timezone.php
@@ -24,6 +24,18 @@ class UnitTestCase_Timezone extends UnitTestCase {
 		$this->assertEqual($timezone->getName(), date_default_timezone_get());
 
 	}
+	
+	public function testTimezoneOffsetGetters() {
+	
+		$timezone = new qCal_Time_Timezone("America/Los_Angeles");
+		$this->assertEqual($timezone->getOffset(), "-08:00");
+		$this->assertEqual($timezone->getOffsetHours(), "-0800");
+		$this->assertEqual($timezone->getOffsetSeconds(), "-28800");
+		$this->assertEqual($timezone->getAbbreviation(), "PST");
+		$this->assertEqual($timezone->isDaylightSavings(), false);
+		$this->assertEqual($timezone->getName(), "America/Los_Angeles");
+	
+	}
 
 	public function testTimezoneSetToGMT() {
 
