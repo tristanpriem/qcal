ba922d54e2734b5382185ea41b6faa71060e9706
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-12-19 05:20:07 +0000

    Added timestamp tests for qCal_Time

--- trunk/lib/qCal/Time.php
+++ trunk/lib/qCal/Time.php
@@ -1,6 +1,24 @@
 <?php
 class qCal_Time {
 
+	/**
+	 * Similar to unix timestamp, but starting from the beginning of the day.
+	 * This class doesn't know what the date is, so it cannot use a unix timestamp.
+	 */
+	protected $timestamp;
+	/**
+	 * The hour in 24-hour format
+	 */
+	protected $hour;
+	/**
+	 * The minute 0-59
+	 */
+	protected $minute;
+	/**
+	 * The second 0-59
+	 */
+	protected $second;
+	
 	public function __construct($hour = null, $minute = null, $second = null) {
 	
 		$this->setTime($hour, $minute, $second);
@@ -9,7 +27,34 @@ class qCal_Time {
 	
 	public function setTime($hour = null, $minute = null, $second = null) {
 	
+		if (is_null($hour)) {
+			$hour = date("G");
+		}
+		if (is_null($minute)) {
+			$minute = date("i");
+		}
+		if (is_null($second)) {
+			$second = date("s");
+		}
+		$this->hour = $hour;
+		$this->minute = $minute;
+		$this->second = $second;
 		
+		// now set the timestamp
+		$time = $this->hour * 60 * 60;
+		$time = $time + $this->minute * 60;
+		$time = $time + $this->second;
+		$this->timestamp = $time;
+	
+	}
+	
+	/**
+	 * Because this is not associated with any certain date, it can only return a
+	 * timestamp starting from the beginning of the day. This is what does that.
+	 */
+	public function getTimestamp() {
+	
+		return $this->timestamp;
 	
 	}
 
--- trunk/tests/UnitTestCase/Time.php
+++ trunk/tests/UnitTestCase/Time.php
@@ -19,12 +19,39 @@ class UnitTestCase_Time extends UnitTestCase {
 	}
 	/**
 	 * Test that time defaults to now
+	 * @note There is a very minor chance of this test failing if it happens to be called at 11:59:58... probably won't happen though.
+	 * @note Commented out because it takes too long and it's really not that important of a test
 	 */
-	public function xtestTimeDefaultsToNow() {
+	/*public function testTimeDefaultsToNow() {
 	
+		$today = date("Y/m/d");
+		$today = strtotime($today);
+		$before = time() - $today;
+		sleep(1);
+		$now = time() - $today;
+		sleep(1);
 		$time = new qCal_Time();
-		$now = time();
-		$this->assertEqual($this->assertEqual());
+		sleep(1);
+		$after = time() - $today;
+		pr("before: $before");
+		pr("now: $now");
+		pr("after: $after");
+		$this->assertTrue(($time->getTimestamp() > $before && $time->getTimestamp() < $after));
+	
+	}*/
+	/**
+	 * test that timezone defaults to server's timezone
+	 */
+	public function testTimestampIsHowManySecondsSinceSecondZeroOfToday() {
+	
+		$today = strtotime(date("Y/m/d"));
+		$now = strtotime(date("Y/m/d G:i:s"));
+		$nowhour = date("G", $now);
+		$nowminute = date("i", $now);
+		$nowsecond = date("s", $now);
+		$diff = $now - $today;
+		$time = new qCal_Time($nowhour, $nowminute, $nowsecond);
+		$this->assertEqual($time->getTimestamp(), $diff);
 	
 	}
 
