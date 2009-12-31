ec7fb650063217ba6c8dc69f7d8615fcb5da4669
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2009-12-31 06:24:42 +0000

    Tagging the 0.0.1 release of qCal

---
+++ tags/release-0.0.1/lib/qCal/Loader.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * qCal_Loader
+ * Loads files from the file system. Looks through the entire include path
+ */
+class qCal_Loader {
+
+	/**
+	 * Loads a file or throws an exception
+	 */
+	static public function loadFile($filename) {
+	
+		if (!self::fileExists($filename)) {
+			throw new qCal_Exception_InvalidFile("$filename does not exist.");
+		}
+		require_once $filename;
+	
+	}
+	/**
+	 * Looks through the include path for file name
+	 */
+	static public function fileExists($filename) {
+	
+		$includepath = get_include_path();
+		$includepath = explode(PATH_SEPARATOR, $includepath);
+		foreach ($includepath as $path) {
+			$path = rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+			if (file_exists($path . $filename)) return true;
+		}
+		return false;
+	
+	}
+
+}
