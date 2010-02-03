d86279ea418f27da0fad549b358947c0dcee4d21
Luke.Visinoni <Luke.Visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2010-02-03 15:25:38 +0000

    Added Recur folder with Yearly recurrence frequency clasS

---
+++ trunk/lib/qCal/DateTime/Recur/Yearly.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Yearly Date/Time Recurrence object.
+ * This class is used to create recurrence rules that happen on a yearly basis.
+ * 
+ * @package qCal
+ * @subpackage qCal_DateTime_Recur
+ * @copyright Luke Visinoni (luke.visinoni@gmail.com)
+ * @author Luke Visinoni (luke.visinoni@gmail.com)
+ * @license GNU Lesser General Public License
+ */
+class qCal_DateTime_Recur_Yearly extends qCal_DateTime_Recur {
+
+	// @todo: implement the required methods...
+
+}
