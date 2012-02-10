91455224f3ec849ea589fae452a5b19b0d2b4db9
luke.visinoni <luke.visinoni@a700fd35-fd3a-0410-b2a1-2143bbc55de5>
2012-02-10 22:32:00 +0000

    Added Date::getXthWeekdayOfMonth

--- branches/namespaced/lib/qCal/DateTime/Date.php
+++ branches/namespaced/lib/qCal/DateTime/Date.php
@@ -19,6 +19,30 @@ class Date extends Base {
     
     protected $_allowedFormatLetters = 'dDjlNSwzWFmMntLoYy';
     
+    static protected $_weekDays = array(
+        0 => 'sunday',
+        1 => 'monday',
+        2 => 'tuesday',
+        3 => 'wednesday',
+        4 => 'thursday',
+        5 => 'friday',
+        6 => 'saturday',
+    );
+    
+    /**
+     * @var Array A list of weekdays and associated numbers in accordance with
+     *            the RFC. 0 = Sunday, 6 = Saturday
+     */
+    static protected $_abbrWeekDays = array(
+        'SU' => 'sunday',
+        'MO' => 'monday',
+        'TU' => 'tuesday',
+        'WE' => 'wednesday',
+        'TH' => 'thursday',
+        'FR' => 'friday',
+        'SA' => 'saturday',
+    );
+    
     public function __construct($year = null, $month = null, $day = null) {
     
         if (is_null($year) || is_null($month) || is_null($day)) {
@@ -371,6 +395,108 @@ class Date extends Base {
      * @access public
      */
     public function getUnixTimestamp() {
+    
+        
+    
+    }
+    
+    /**
+     * Converts either whole or abbreviated weekday name to its associated number
+     */
+    static public function weekDayToNum($weekDayName) {
+    
+        $upper = strtoupper($weekDayName);
+        $lower = strtolower($weekDayName);
+        $weekdays = array_flip(self::$_weekDays);
+        if (array_key_exists($upper, self::$_abbrWeekDays)) {
+            $weekday = self::$_abbrWeekDays[$upper];
+            return $weekdays[$weekday];
+        } elseif (array_key_exists($lower, $weekdays)) {
+            return $weekdays[$lower];
+        }
+    
+    }
+    
+    /**
+     * Date magic
+     * This class is capable of doing some really convenient things with dates.
+     * It is capable of determining things such as how many days until the end of the year,
+     * which monday of the month it is (ie: third monday in february), etc.
+     */
+    
+    /**
+     * Determine the number of Tuesdays (or whatever day of the week this date is) since the
+     * beginning or end of the month.
+     * @param integer $xth A positive or negative number that determines which weekday of the month we want
+     * @param string|integer $weekday Either Sunday-Saturday or 0-6 to specify the weekday we want (can also be SU through SA)
+     * @param string|integer $month Either January-December or 1-12 to specify the month we want
+     * @param integer $year A valid year to specify which year we want
+     * @return qCal\DateTime\Date An object representing the xth day of the month
+     * @access public
+     */
+    static public function getXthWeekdayOfMonth($xth, $weekday = null, $month = null, $year = null) {
+    
+        $negpos = substr($xth, 0, 1);
+        if ($negpos == "+" || $negpos == "-") {
+            $xth = (integer) substr($xth, 1);
+        } else {
+            $negpos = "+";
+        }
+        $firstofmonth = new Date($year, $month, 1);
+        $numdaysinmonth = $firstofmonth->getNumDaysInMonth();
+        $numweekdays = 0; // the number of weekdays that have occurred (in the loop)
+        $foundday = false;
+        if ($negpos == '+') {
+            $day = 1;
+            $wday = $firstofmonth->getWeekday();
+            while ($day <= $numdaysinmonth) {
+                if ($weekday == $wday) {
+                    $numweekdays++;
+                    if ($numweekdays == $xth) {
+                        // We found the Xth weekday! 
+                        $foundday = $day;
+                        break;
+                    }
+                }
+                if ($wday == 6) $wday = 0; // after Saturday reset to Sunday
+                else $wday++;
+                $day++;
+            }
+        } else {
+            $day = $numdaysinmonth;
+            $lastofmonth = $firstofmonth->getLastDayOfMonth();
+            $wday = $lastofmonth->getWeekday();
+            while ($day >= 1) {
+                if ($weekday == $wday) {
+                    $numweekdays++;
+                    if ($numweekdays == $xth) {
+                        // We found the Xth weekday!
+                        $foundday = $day;
+                        break;
+                    }
+                }
+                if ($wday == 0) $wday = 6; // reset week
+                else $wday--;
+                $day--;
+            }
+        }
+        if ($foundday && checkdate($month, $foundday, $year)) {
+            return new Date($year, $month, $foundday);
+        }
+        // No day found, throw exception
+    
+    }
+    
+    /**
+     * Determine the number or Tuesdays (or whatever day of the week this date is) since the
+     * beginning or end of the year.
+     * @param integer $xth A positive or negative number that determines which weekday of the year we want
+     * @param string|integer $weekday Either Sunday-Saturday or 0-6 to specify the weekday we want (can also be SU through SA)
+     * @param integer $year A valid year to specify which year we want
+     * @return qCal_Date An object representing the xth day of the month
+     * @access public
+     */
+    public function getXthWeekdayOfYear($xth, $weekday = null, $year = null) {
     
         
     
